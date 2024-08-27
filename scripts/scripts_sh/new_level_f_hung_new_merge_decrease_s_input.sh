e6bd89978ab2650c40d0067b60980e7781dac2f2
baoht <baoht@vng.com.vn>
2024-08-27 08:51:50 +0700

    more train

--- scripts/scripts_sh/new_level_f_hung_new_merge_decrease_s_input.sh
+++ scripts/scripts_sh/new_level_f_hung_new_merge_decrease_s_input.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 python training.py \
-    --prefix_name 85gamma_new_levels_decrease_s_input_p256_180_180_161_v256_256_32 \
+    --prefix_name self_attention_85gamma_new_levels_input \
     --pi 256 180 180 161 \
     --vf 256 256 32 \
     --obs-order none_tile color_1 color_2 color_3 color_4 color_5 pu disco bomb missile_h missile_v plane blocker monster monster_match_dmg_mask monster_inside_dmg_mask self_dmg_mask legal_action \
