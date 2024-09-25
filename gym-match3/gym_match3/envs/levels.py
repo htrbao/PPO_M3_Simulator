a3734c1b8ac915e3b8d05bc41cb6576d2f3b52d0
Ha Thien Loc <ha.thienloc131@gmail.com>
2024-09-25 09:33:40 +0700

    shuffle easy levels

--- gym-match3/gym_match3/envs/levels.py
+++ gym-match3/gym_match3/envs/levels.py
@@ -172,8 +172,8 @@ class Match3Levels:
 
 
 easy_levels = []
-for x in range(1, 9):
-    for y in range(1, 8):
+for x in range(0, 9):
+    for y in range(0, 8):
         easy_board = [[0 for _ in range(9)] for _ in range(10)]
         for _x in range(x, x + 2):
             for _y in range(y, y + 2):
@@ -229,7 +229,7 @@ for x in range(1, 9):
                 ],
             )
         )
-
+random.shuffle(easy_levels)
 LEVELS = [
     *easy_levels,
     Level(
