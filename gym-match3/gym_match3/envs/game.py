9a2f7cbd81cee166819dd016d028ad5de8832e2e
baoht <105-baoht@users.noreply.git-logteam.zingplay.com>
2024-10-03 14:08:01 +0700

    [Back] back to calc attack reward with num_tiles

--- gym-match3/gym_match3/envs/game.py
+++ gym-match3/gym_match3/envs/game.py
@@ -1659,8 +1659,10 @@ class Game(AbstractGame):
         for i in range(len(self.list_monsters)):
             near_monster = min(near_monster, point.euclidean_distance(self.list_monsters[i]._position))
             match_damage, pu_damage = self.list_monsters[i].get_dame(matches, inside_brokens, disco_brokens)
-            total_match_dmg += match_damage / self.list_monsters[i]._origin_hp
-            total_power_dmg += pu_damage / self.list_monsters[i]._origin_hp
+            # total_match_dmg += match_damage / self.list_monsters[i]._origin_hp
+            # total_power_dmg += pu_damage / self.list_monsters[i]._origin_hp
+            total_match_dmg += match_damage
+            total_power_dmg += pu_damage
             score -= pu_damage
 
             self.list_monsters[i].attacked(match_damage, pu_damage)
