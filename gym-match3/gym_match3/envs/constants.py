b353bccabaa1b0fb3a112984cb58341a2624e5a9
baoht <105-baoht@users.noreply.git-logteam.zingplay.com>
2024-10-15 17:14:29 +0700

    [Update] Big fix

--- gym-match3/gym_match3/envs/constants.py
+++ gym-match3/gym_match3/envs/constants.py
@@ -6,7 +6,6 @@ from collections import namedtuple
 Level = namedtuple("Level", ["h", "w", "n_shapes", "board", "list_monsters"])
 base_hp = 5
 
-
 @dataclass(frozen=True)
 class GameObject:
     immovable_shape = -1
@@ -45,6 +44,32 @@ class GameObject:
     set_unmovable_shape = set_monsters_shape | set_blockers_shape
     set_unmovable_shape.add(immovable_shape)
     set_movable_shape = set_tiles_shape | set_powers_shape
+
+
+@dataclass(frozen=True)
+class GameAction:
+    swap_normal = 0
+    swap_4_v = 1
+    swap_4_h = 2
+    swap_L = 3
+    swap_T = 4
+    swap_2x2 = 5
+    swap_5 = 6
+    swap_power_missile_h = 7
+    swap_power_missile_v = 8
+    swap_power_bomb = 9
+    swap_power_plane = 10
+    swap_power_disco = 11
+    swap_merge_missile_missile = 12
+    swap_merge_missile_bomb = 13
+    swap_merge_missile_plane = 14
+    swap_merge_missile_disco = 15
+    swap_merge_bomb_bomb = 16
+    swap_merge_bomb_plane = 17
+    swap_merge_bomb_disco = 18
+    swap_merge_plane_plane = 19
+    swap_merge_plane_disco = 20
+    swap_merge_disco_disco = 21
 
 
 def mask_immov_mask(line, immovable_shape, can_move_blocker=False):
