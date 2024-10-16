eba758377bf656066c0285f50f74b03d99261a87
Hồ Hải Thủy <HHT.ThuyHH@gmail.com>
2024-10-16 16:23:17 +0700

    [Podman] Update run file and docker-compose

--- podman/run.sh
+++ podman/run.sh
@@ -1,21 +1,27 @@
 ##!/bin/bash
-#git reset --hard HEAD
 git pull
-echo $BATCH_SIZE
-echo $LEARNING_RATE
-echo $GAMMA
-echo $NUM_ENVS
 
 python training.py \
   --prefix_name milestone_mha_penalty_not_both_harder_ta_advice \
-  --strategy milestone \
-  --pi 256 128 128 128 128 128 128 128 128 128 \
-  --vf 4096 256 256 256 256 256 256 256 256 256 \
-  --num_heads 2 \
-  --n_steps 131072 \
-  --obs-order none_tile color_1 color_2 color_3 color_4 color_5 pu disco bomb missile_h missile_v plane blocker monster monster_match_dmg_mask monster_match_hp monster_inside_dmg_mask monster_inside_hp self_dmg_mask match_normal match_2x2 match_4_v match_4_h match_L match_T match_5 legal_action heat_mask \
-  --batch_size $BATCH_SIZE \
-  --lr $LEARNING_RATE \
-  --gamma $GAMMA \
-  --n_updations 1 \
-  --num_envs $NUM_ENVS >>"logging/$MODEL_NAME.log"
+  --kernel_size "$KERNEL_SIZE" \
+  --mid_channels "$MID_CHANNELS" \
+  --num_first_cnn_layer "$NUM_FIRST_CNN_LAYER" \
+  --num_self_attention_layers "$NUM_SELF_ATTENTION_LAYERS" \
+  --strategy "$STRATEGY" \
+  --pi "$PI" \
+  --vf "$VF" \
+  --epochs "$EPOCHS" \
+  --ent_coef "$ENT_COEF" \
+  --num_heads "$NUM_HEADS" \
+  --n_steps "$N_STEPS" \
+  --obs-order "$OBS_ORDER" \
+  --batch_size "$BATCH_SIZE" \
+  --lr "$LEARNING_RATE" \
+  --gamma "$GAMMA" \
+  --n_updations "$N_UPDATIONS" \
+  "${EXTRA1:-}" \
+  "${EXTRA2:-}" \
+  "${EXTRA3:-}" \
+  "${EXTRA4:-}" \
+  "${EXTRA5:-}" \
+  --num_envs "$NUM_ENVS" >>"logging/$MODEL_NAME.log"
